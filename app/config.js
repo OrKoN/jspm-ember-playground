bef135819e3e006843ee0a6ae46a1b0c4cb620fb
Oleksii Rudenko <oleksii.rudenko@n-fuse.de>
2015-02-24 11:10:57 +0100

    Ember 1.11.0-beta.2

--- app/config.js
+++ app/config.js
@@ -8,7 +8,16 @@ System.config({
 
 System.config({
   "map": {
-    ehbs: "github:n-fuse/plugin-ember-hbs/ember"
+    "ehbs": "github:n-fuse/plugin-ember-hbs/ember",
+    "ember": "github:components/ember@1.11.0-beta.2",
+    "hbs": "github:n-fuse/plugin-ember-hbs@1.11.0-beta.2",
+    "jquery": "github:components/jquery@2.1.3",
+    "github:components/ember@1.11.0-beta.2": {
+      "jquery": "github:components/jquery@2.1.3"
+    },
+    "github:n-fuse/plugin-ember-hbs@1.11.0-beta.2": {
+      "ember-template-compiler": "github:n-fuse/ember-template-compiler@1.11.0-beta.2"
+    }
   }
 });
 

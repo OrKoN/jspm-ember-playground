de876eddb6b3c3cb09430babb0741174d9c3d841
Oleksii Rudenko <OrKoN@users.noreply.github.com>
2015-02-24 11:06:54 +0100

    Update config.js

--- app/config.js
+++ app/config.js
@@ -8,13 +8,7 @@ System.config({
 
 System.config({
   "map": {
-    "ehbs": "github:n-fuse/plugin-ember-hbs/ember",
-    "components/ember": "github:components/ember@1.11.0-beta.2",
-    "ember": "github:components/ember@1.11.0-beta.2",
-    "jquery": "github:components/jquery@2.1.3",
-    "github:components/ember@1.11.0-beta.2": {
-      "jquery": "github:components/jquery@2.1.3"
-    }
+    "ehbs": "plugin-ember-hbs/ember"
   }
 });
 

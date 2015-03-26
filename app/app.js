6cc74ea9d38cf5a061776f06b0233ccc8217bb8d
Oleksii Rudenko <oleksii.rudenko@n-fuse.de>
2015-03-26 10:11:41 +0100

    Updated to work with jspm 0.15.0-beta

--- app/app.js
+++ app/app.js
@@ -1,5 +1,6 @@
 import Ember from 'ember';
-import tmpl from 'templates/application.hbs!ihbs'; // loaded into Ember.TEMPLATES as 'application'
+import tmpl from 'templates/application.hbs!'; // loaded into Ember.TEMPLATES as 'application'
+Ember.TEMPLATES.application = tmpl;
 
 import './components/my-component';
 var Demo = Ember.Application.create();
--- gulpfile.js
+++ gulpfile.js
@@ -13,15 +13,24 @@ gulp.task('watch', function() {
 
   gulp.watch([
     'app/**/*.*'
-  ], function(file){
+  ], function(file) {
     browserSync.reload(file.path);
   });
 });
 
 gulp.task('jspm', function() {
-  return jspm.bundleSFX('main', 'dist/app.js', { sourceMaps: false });
+  return jspm.bundleSFX('app', 'dist/app.js', { sourceMaps: false, minify: true, mangle: false });
 });
 
-gulp.task('build', [
-  'jspm'
-]);
+// gulp.task('jspm', function() {
+//   return jspm.bundle('app - app/**/*.hbs', 'dist/app.js', { sourceMaps: false });
+// });
+
+// gulp.task('build', [
+//   'jspm',
+//   'jspm-templates'
+// ]);
+
+// gulp.task('jspm-templates', function() {
+//   return jspm.bundle('app/**/*.hbs', 'dist/app-templates.js', { sourceMaps: false });
+// });

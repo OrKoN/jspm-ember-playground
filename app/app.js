68659edec15b19f2332ddf3ed55aa7fda50521d8
Oleksii Rudenko <oleksii.rudenko@n-fuse.de>
2015-03-30 13:37:28 +0200

    Ember 1.12

--- app/app.js
+++ app/app.js
@@ -2,5 +2,7 @@ import Ember from 'ember';
 import tmpl from 'templates/application.hbs!'; // loaded into Ember.TEMPLATES as 'application'
 Ember.TEMPLATES.application = tmpl;
 
+import 'ember-vcl-calendar';
+
 import './components/my-component';
 var Demo = Ember.Application.create();

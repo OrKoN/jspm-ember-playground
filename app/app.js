f9358bcf7c8423c0443ad8637d38d9e3341a03d3
Oleksii Rudenko <oleksii.rudenko@n-fuse.de>
2015-04-14 16:16:10 +0200

    App Update

--- app/app.js
+++ app/app.js
@@ -6,14 +6,4 @@ Ember.TEMPLATES.application = tmpl;
 
 import './components/my-component';
 var Demo = Ember.Application.create({
-  IndexRoute: Ember.Route.extend({
-    afterModel: function(resolvedModel, transition) {
-      var _this = this;
-
-      // keep a reference for usage in then()
-      transition.then(function() {
-        console.log(_this.get('router.url'));
-      });
-    }
-  })
 });

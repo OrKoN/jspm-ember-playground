366dce3005cdfb6fb40dbf1beead7464b8d64557
Oleksii Rudenko <alexei.rudenko@gmail.com>
2015-04-23 21:42:16 +0200

    Updated and test with jspm 0.15.4

--- app/config.js
+++ app/config.js
@@ -15,9 +15,9 @@ System.config({
 
 System.config({
   "map": {
-    "babel": "npm:babel-core@5.1.8",
-    "babel-runtime": "npm:babel-runtime@5.1.8",
-    "core-js": "npm:core-js@0.8.3",
+    "babel": "npm:babel-core@5.1.11",
+    "babel-runtime": "npm:babel-runtime@5.1.11",
+    "core-js": "npm:core-js@0.8.4",
     "ember": "github:components/ember@1.12.0-beta.1",
     "ember-computed-decorators": "github:rwjblue/ember-computed-decorators@master",
     "hbs": "github:n-fuse/plugin-ember-hbs@1.12.0-beta.1",
@@ -36,9 +36,13 @@ System.config({
       "ember-template-compiler": "github:n-fuse/ember-template-compiler@1.12.0-beta.1"
     },
     "github:rwjblue/ember-computed-decorators@master": {
+      "ember": "github:components/ember@1.12.0-beta.1",
+      "ember-new-computed": "github:rwjblue/ember-new-computed@master"
+    },
+    "github:rwjblue/ember-new-computed@master": {
       "ember": "github:components/ember@1.12.0-beta.1"
     },
-    "npm:core-js@0.8.3": {
+    "npm:core-js@0.8.4": {
       "process": "github:jspm/nodelibs-process@0.1.1"
     }
   }

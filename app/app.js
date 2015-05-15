f71b2dd28f1331104c90d82ee46ade38a51f404f
Oleksii Rudenko <oleksii.rudenko@n-fuse.de>
2015-05-15 12:20:49 +0200

    1.13.0-beta.1

--- app/app.js
+++ app/app.js
@@ -4,8 +4,6 @@ import tmpl from 'templates/application.hbs!';
 // loaded into Ember.TEMPLATES as 'application'
 Ember.TEMPLATES.application = tmpl;
 
-import Pajax from 'pajax';
-
 import './components/my-component';
 var Demo = Ember.Application.create({
 });
